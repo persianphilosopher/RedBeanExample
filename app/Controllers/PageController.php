1183e55d25b9b95f26be8358fee6ef235377bfc3
fe-198.27.65.85 <farhang.erfani@gmail.com>
2018-03-09 02:34:18 -0500

    minor-tweak

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -9,17 +9,17 @@ class PageController {
 
     public function __construct(){
 		View::addDirectory(ROOT.'/app/Views');
-		R::setup( 'sqlite:'.SQLOCATION );
+		R::setup('sqlite:'.SQLOCATION);
     }
     public function home(){
-		$users = R::getAll( 'SELECT * from yay' );
+		$users = R::getAll( 'SELECT * from user_bean' );
 		\Dabl\View\View::load('home', ['users'=>$users]);
 	}
 	public function getAdd(){
 		\Dabl\View\View::load('post');
 	}
 	public function postAdd(){
-	    $user = R::dispense( 'yay' );
+	    $user = R::dispense( 'user_bean' );
         $user->name = $_POST['name'];
         $user->email = $_POST['email'];
         $user->password = $_POST['password'];
@@ -29,18 +29,18 @@ class PageController {
 	}
 	public function getEdit($url){
 		$id=$url;
-		$users = R::load( 'yay', $id);
+		$users = R::load( 'user_bean', $id);
 		\Dabl\View\View::load('edit', ['users'=>$users]);
 	}
 	public function postEdit($url){
-	    $users = R::load( 'yay', $url );
+	    $users = R::load( 'user_bean', $url );
     	$users->name=$_POST['name'];
     	$users->email=$_POST['email'];
     	R::store($users);
     	header("Location:/");
 	}
 	public function delete($url){
-    	$users = R::load( 'yay', $url );
+    	$users = R::load( 'user_bean', $url );
     	R::trash( $users);
 		header("Location:/");
 	}
